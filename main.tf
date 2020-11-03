9f7cb1632205f24f5fe3e6ac464f2269d094260b
ghazal-naderi <ghazal.naderi.360@gmail.com>
2020-11-03 11:28:04 +0000

    Removed readme and added .tf files

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+    backend "remote" {
+        hostname = "app.terraform.io"
+        organization = "ghazalnaderi"
+
+        workspaces {
+            name = "udemy_test"
+        }
+    }
+}
+
+provider "aws"{
+    region = "us-east-1"
+    access_key = var.access_key
+    secret_key = var.secret_key
+}
+
+resource "aws_s3_bucket" "test" {
+    bucket_prefix = "test-"
+
+}
